91b8ee15b6108f7f61638719f4b886094d9e9d5b
Fernando Herrera <52893380+herreralemus03@users.noreply.github.com>
2022-01-15 18:31:20 -0600

    Merge branch changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
 
+<<<<<<< HEAD
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
 import { DbzModule } from './dbz/dbz.module';
+=======
+>>>>>>> parent of 2d67ccc (primer commit)
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContadorComponent,
+    HeroeComponent,
+    ListadoComponent
   ],
   imports: [
+<<<<<<< HEAD
     BrowserModule,
     HeroesModule,
     ContadorModule,
     DbzModule
+=======
+    BrowserModule
+>>>>>>> parent of 2d67ccc (primer commit)
   ],
   providers: [],
   bootstrap: [AppComponent]
